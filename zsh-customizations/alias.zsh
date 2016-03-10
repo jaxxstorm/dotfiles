39fd332aeda665b62f3a0ef75c2d9531aca342bc
Lee Briggs <lee@leebriggs.co.uk>
2016-03-10 21:25:00 +0000

    Alias vim to homebrew version

--- zsh-customizations/alias.zsh
+++ zsh-customizations/alias.zsh
@@ -1,5 +1,6 @@
 # Custom aliases I'm using
 alias gitclean_preview="git branch --merged | grep -v '\*'"
 alias gitclean="git branch --merged | grep -v '\*' | xargs -n 1 git branch -d"
+alias vim="/usr/local/bin/vim"
 #alias ssh="/usr/local/bin/mosh"
 #alias sssh="/usr/bin/ssh"
