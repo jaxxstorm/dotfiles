25a77a1b6a83a90945000affd1c9600bc4a5e008
Lee Briggs <lee@leebriggs.co.uk>
2016-04-20 07:26:20 +0100

    More verbose output

--- zsh-customizations/jira.zsh
+++ zsh-customizations/jira.zsh
@@ -6,7 +6,7 @@ source ~/.jira
 
 function jira_api_get_customfield () {
   if [[ -n "$1" ]]; then
-    curl -u lbriggs -X GET -H "Content-Type: application/json" ${JIRA_URL}/rest/api/2/issue/createmeta\?projectKeys\=OPS\&issuetypeNames\=OPS%20Ticket\&expand\=projects.issuetypes.fields | jq ".projects[] | .issuetypes[] | .fields | .${1} | .name,.allowedValues[]"
+    curl -u lbriggs -X GET -H "Content-Type: application/json" ${JIRA_URL}/rest/api/2/issue/createmeta\?projectKeys\=OPS\&issuetypeNames\=OPS%20Ticket\&expand\=projects.issuetypes.fields | jq ".projects[] | .issuetypes[] | .fields | .${1}"
   else
     echo "Must pass custom field name"
   fi
