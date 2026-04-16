0db5f3cf8fd456df6d7bfc3aa62d9501f27fd67e
Lee Briggs <lee@leebriggs.co.uk>
2026-04-15 18:13:43 -0700

    add more tools from mise
    Signed-off-by: Lee Briggs <lee@leebriggs.co.uk>

---
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,11 @@
+alias ls="eza"
+#alias cat="bat --style=plain --paging=never"
+
+alias p="pulumi"
+alias pulumy="pulumi"
+alias plumy="pulumi"
+if [[ "$OSTYPE" == darwin* ]]; then
+  alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
+fi
+#alias vim="nvim"
+#alias kubectx='switch'
